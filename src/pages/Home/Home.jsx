6f60fb778bd730299faa30911484342b07cdbebc
JoacoGramigna <joacogramigna@gmail.com>
2023-05-24 04:16:44 -0300

    Add content to aboutUs section
    Change the use of color values by css variables

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -28,32 +28,53 @@ const Home = () => {
 
   return (
     <>
-      <div className="backgroundHome"></div>
-      <h1 className="sloganHome">
-        Viví la experiencia de viajar a la Patagonia.
-      </h1>
-      <div className={isMoved ? "planeIconActive" : "planeIcon"}>
-        <img className="planeImage" src={PlaneIcon} alt="Avión" />
-      </div>
-      <section className="homePage">
+      <section className="home">
+        <div className="backgroundHome"></div>
+        <h1 className="sloganHome">
+          Viví la experiencia de viajar a la Patagonia.
+        </h1>
+        <div className={isMoved ? "planeIconActive" : "planeIcon"}>
+          <img className="planeImage" src={PlaneIcon} alt="Avión" />
+        </div>
+      </section>
+      <section className="mainPage">
         <section className="whyChooseUs">
-            <h2>¿Por qué elegirnos?</h2>
-            <div className="boxCenter">
+          <h2>¿Por qué elegirnos?</h2>
+          <div className="boxCenter">
             <article className="boxToRight">
               <div className="box" data-aos="fade-right"></div>
             </article>
             <article className="boxToLeft">
               <div className="box" data-aos="fade-left"></div>
             </article>
-            </div>
-            <div className="boxCenter">
+          </div>
+          <div className="boxCenter">
             <article className="boxToRight">
               <div className="box" data-aos="fade-right"></div>
             </article>
             <article className="boxToLeft">
               <div className="box" data-aos="fade-left"></div>
             </article>
-            </div>
+          </div>
+        </section>
+        <section className="aboutUs">
+          <article className="infoAboutUs">
+            <h2>Sobre nosotros</h2>
+            <h3>La mejor agencia de viajes a la Patagonia desde 2021</h3>
+            <p className="paragraph0">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Necessitatibus, temporibus. Nobis distinctio at enim quia
+              assumenda, eveniet corrupti numquam est!
+            </p>
+            <p className="paragraph"><i>+</i> Los mejores precios y ofertas.</p>
+            <p className="paragraph"><i>+</i> Las guías mas completas para tus visitas.</p>
+            <p className="paragraph"><i>+</i> Packs de viajes con todo incluido.</p>
+            <button className="moreInfoButton">Ver mas</button>
+          </article>
+          <article className="imageAboutUs">
+            <div className="image1" />
+            <div className="image2" />
+          </article>
         </section>
       </section>
     </>
